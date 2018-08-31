0b0aace5f9c473417dcf87088448695dac89d329
Brendon Pagano <brendonpagano@gmail.com>
2018-08-31 19:03:15 -0300

    Configuring webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,21 @@
 // Node Modules
 const path = require('path');
 
-module.exports = {
-    mode: 'development',
+// Webpack Plugins
+const webpack = require('webpack');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
+
+
+module.exports = {
     entry: {
-        app: path.resolve(__dirname, 'src/index.js')
+        app: path.resolve(__dirname, 'src', 'index.js')
     },
 
     output: {
-        filename: '[name].min.js',
+        filename: '[name].[chunkhash].min.js',
         path: path.resolve(__dirname, 'build')
     },
 
@@ -18,9 +24,48 @@ module.exports = {
             { test: /\.css$/, use: 'css-loader' },
             {
                 test: /\.js$/,
+                include: path.resolve(__dirname, 'src'),
                 exclude: /node_modules/,
                 use: ['babel-loader']
             }
+        ]
+    },
+
+    plugins: [
+        new CleanWebpackPlugin('build'),
+        new webpack.DefinePlugin({
+            'CANVAS_RENDERER': JSON.stringify(true),
+            'WEBGL_RENDERER': JSON.stringify(true)
+        }),
+        new HtmlWebpackPlugin({
+            path: path.resolve(__dirname, 'build', 'index.html'),
+            template: 'index.html'
+        })
+    ],
+
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                vendors: false,
+                default: false,
+                vendor: {
+                    chunks: 'all',
+                    test: /node_modules/
+                }
+            }
+        },
+        minimizer: [
+            new UglifyJsPlugin({
+                sourceMap: true,
+                uglifyOptions: {
+                    ecma: 8,
+                    compress: true,
+                    output: {
+                        comments: false,
+                        beautify: false,
+                    }
+                }
+            })
         ]
     },
 
